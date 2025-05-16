e0d0a347a8a2e62fabe77e5824ea39494e0832ed
Marcos Sousa <pelaez.marcos@gmail.com>
2025-05-16 16:32:32 -0300

    Acessando itens de cardápio

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'goumert')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 5)
 
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
