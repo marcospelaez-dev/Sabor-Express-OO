cbbdd531228a0e15d78aad41d974302381c8a0f9
Marcos Sousa <pelaez.marcos@gmail.com>
2025-05-16 11:06:12 -0300

    Aprofundando propriedades

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,20 +2,20 @@ class Restaurante:
     restaurantes = []
     # Padrão é "self", mas o Python aceita o "this" ou qualquer outro nome que se queira dar
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
 
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         print()
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
