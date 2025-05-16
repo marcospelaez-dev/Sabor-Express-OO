4b126a113faeff5d45b3f347f1b674f62f6f2c66
Marcos Sousa <pelaez.marcos@gmail.com>
2025-05-16 10:22:34 -0300

    Métodos especiais init str

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,17 @@
 class Restaurante:
+    # Padrão é "self", mas o Python aceita o "this" ou qualquer outro nome que se queira dar
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Pizzaria')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+print(restaurante_praca)
+print(restaurante_pizza)
