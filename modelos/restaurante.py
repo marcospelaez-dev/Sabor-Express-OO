f2b7bc827a51d36616cbce93e9b582995d050dcf
Marcos Sousa <pelaez.marcos@gmail.com>
2025-05-16 11:14:27 -0300

    Diferentes métodos
    Métodos de classe e métodos de instância

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -9,19 +9,25 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self._nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         print()
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '☑' if self._ativo else '☐'
+    
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Pizzaria')
 
 Restaurante.listar_restaurantes()
