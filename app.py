bb08f126fdeb7ef0b8d4176552bf7bf31f8d4ade
Marcos Sousa <pelaez.marcos@gmail.com>
2025-05-18 18:49:54 -0300

    Exibindo cardápio

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
